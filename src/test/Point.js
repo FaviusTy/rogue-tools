6c1b3cc250fea154532e6fb5a5c42d6414eeccbb
favi_ty <favius983@gmail.com>
2018-01-02 19:04:51 +0900

    fixed Point.key method.

--- src/test/Point.js
+++ src/test/Point.js
@@ -8,3 +8,8 @@ test('Point は引数 x, y の値を持つ', () => {
   assert(point.x === 1)
   assert(point.y === 2)
 })
+
+test('Point.key() でシリアライズされた文字列を受け取れる', () => {
+  const point = new Point(10, 15)
+  assert('10,15' === point.key)
+})
