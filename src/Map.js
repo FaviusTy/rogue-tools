f4f8df037ad5d82435ce80c4081612d123a60d84
favi_ty <favius983@gmail.com>
2018-01-02 20:25:55 +0900

    added range check

--- src/Map.js
+++ src/Map.js
@@ -2,7 +2,11 @@ import either from './utils/either'
 import Point from './Point'
 
 function convert1DPoint(pointer, map) {
-  return either(pointer.y - 1, y => y >= 0)(0) * map.width + pointer.x
+  return either(pointer.y, y => y >= 0)(0) * map.width + pointer.x
+}
+
+function isOverRange(point, map) {
+  return point.x > map.width || point.y > map.height
 }
 
 export default class Map {
@@ -17,10 +21,12 @@ export default class Map {
   }
 
   put(point, value) {
+    if (isOverRange(point, this)) return new Error(`"${point.key}" is over range!`)
     this._row[convert1DPoint(point, this)] = value
   }
 
   pick(point) {
+    if (isOverRange(point, this)) return
     return this._row[convert1DPoint(point, this)]
   }
 }
--- src/test/Map.js
+++ src/test/Map.js
@@ -22,7 +22,7 @@ test('それが生成する空間配列の各要素の初期値はnullである'
 test('それは put によって指定した座標に対応する要素を保持する', () => {
   const map = new Map(5, 5)
   map.put(new Point(3, 3), true)
-  assert(map.row[13])
+  assert(map.row[18])
 })
 
 test('それは pick によって与えられたPointに格納されている値を返す', () => {
@@ -30,4 +30,14 @@ test('それは pick によって与えられたPointに格納されている値
   const point = new Point(1, 2)
   map.put(point, true)
   assert(map.pick(point))
+})
+
+test('それは pick に与えたPointが空間の範囲外だった時にundefinedを返す', () => {
+  const map = new Map(5, 5)
+  const overX = new Point(5, 0)
+  const overY = new Point(1, 5)
+  const overRange = new Point(5, 5)
+  assert(map.pick(overX) === undefined)
+  assert(map.pick(overY) === undefined)
+  assert(map.pick(overRange) === undefined)
 })
