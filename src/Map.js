35a245a90401a27eb80b89bbd7660573c456ac04
favi_ty <favius983@gmail.com>
2018-01-03 10:23:59 +0900

    isOverRange was method

--- src/Map.js
+++ src/Map.js
@@ -9,11 +9,6 @@ function convertRawIndex({ x, y }, { width }) {
   return either(y, isPositive)(0) * width + either(x, isPositive)(0)
 }
 
-function isOverRange({ x, y }, { width, height }) {
-  if (x < 0 || y < 0) return false
-  return x >= width || y >= height
-}
-
 const privates = new WeakMap()
 
 export default class Map {
@@ -37,18 +32,24 @@ export default class Map {
     return [...privates.get(this).raw]
   }
 
+  isOverRange(point) {
+    const { x, y } = point
+    if (x < 0 || y < 0) return false
+    return x >= this.width || y >= this.height
+  }
+
   fill(element) {
     privates.get(this).raw.fill(element)
   }
 
   put(point, value) {
-    if (isOverRange(point, this)) return new Error(`"${point.key}" is over range!`)
+    if (this.isOverRange(point)) return new Error(`"${point.key}" is over range!`)
     privates.get(this).raw[convertRawIndex(point, this)] = value
     console.log(privates.get(this).raw)
   }
 
   pick(point) {
-    if (isOverRange(point, this)) return
+    if (this.isOverRange(point)) return
     return privates.get(this).raw[convertRawIndex(point, this)]
   }
 }
