e2510b52746b0eaf9e9fa30d9359117bdde97026
favi_ty <favius983@gmail.com>
2018-01-03 09:57:46 +0900

    WIP: TyrantBuilder

---
+++ src/DungeonBuilder/Tyrant/index.js
@@ -0,0 +1,30 @@
+import Map from '../Map'
+
+const privates = new WeakMap()
+
+const DEFAULT_OPTS = {
+  dugPercentage: 0.2,
+  roomNumber: 4,
+  minRoomWidth: 3,
+  maxRoomWidth: 9,
+  minRoomHeight: 3,
+  maxRoomHeight: 5,
+  corriderNumber: 4,
+  minCorriderLength: 3,
+  maxCorriderLength: 10,
+}
+
+export default class Tyrant {
+  constructor(map, options = {}) {
+    this.map = map
+    privates.set(this, {
+      rooms: [],
+      corriders: [],
+      walls: [],
+      dug: 0,
+      ...Object.assign({}, DEFAULT_OPTS, options),
+    })
+  }
+
+  *create() {}
+}
