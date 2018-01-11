81903c7a3327f6dbb56c48fc0e54fd4b05b2ac90
favi_ty <favius983@gmail.com>
2018-01-11 20:23:18 +0900

    created simple map rendering.

--- examples/src/firstStep.js
+++ examples/src/firstStep.js
@@ -1,9 +1,14 @@
+import Point from '../../src/Point'
+import { rect } from '../../src/matrix'
 import Map from '../../src/Map'
 
 const dpr = window.devicePixelRatio || 1
 const canvas = document.querySelector('#screen')
 const ctx = canvas.getContext('2d')
-const fontSize = 11
+const fontSize = 14
+
+const map = new Map(60, 30)
+map.fill('.')
 
 function roop() {
   canvas.width = window.innerWidth * dpr
@@ -11,8 +16,9 @@ function roop() {
   ctx.scale(dpr, dpr)
   ctx.font = `${fontSize}px 'Courier New'`
   ctx.fillStyle = 'rgb(255, 255, 255)'
-  'Hello, world!'.split('').forEach((v, i) => ctx.fillText(v, (fontSize / 2 + 1) * i, fontSize))
-  '##################'.split('').forEach((v, i) => ctx.fillText(v, (fontSize / 2 + 1) * i, fontSize * 2))
+  rect(new Point(0, 0), map.width, map.height).forEach(point => {
+    ctx.fillText(map.pick(point), (fontSize / 2 + 1) * point.x, fontSize * point.y)
+  })
   requestAnimationFrame(roop)
 }
 
