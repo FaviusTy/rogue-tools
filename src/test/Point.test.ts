7a87bfdc20a44aed1c5f70e49075ef05bdf78816
Favi_ty <favius983@gmail.com>
2019-12-19 22:15:12 +0900

    wip

--- src/test/Point.test.ts
+++ src/test/Point.test.ts
@@ -7,7 +7,7 @@ test("Point は引数 x, y の値を持つ", () => {
   assert(point.y === 2);
 });
 
-test("Point.key() でシリアライズされた文字列を受け取れる", () => {
+test("Point.key でシリアライズされた文字列を受け取れる", () => {
   const point = new Point(10, 15);
   assert("10,15" === point.key);
 });
