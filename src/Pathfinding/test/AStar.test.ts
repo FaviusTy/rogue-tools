60d4998e542c20b4151f9c41b3931fd84033e7d8
Favi_ty <favius983@gmail.com>
2019-12-20 00:10:53 +0900

    refactored

--- src/Pathfinding/test/AStar.test.ts
+++ src/Pathfinding/test/AStar.test.ts
@@ -1,4 +1,5 @@
 import assert from "power-assert";
+import { stripIndents } from "common-tags";
 import AStar from "../AStar";
 import Map from "../../Map";
 import Point from "../../Point";
@@ -18,34 +19,62 @@ test("Astar", () => {
 
 test("flat routing: Never", () => {
   const map = new Map<number>(5, 7);
+  const matcher = stripIndents`
+  1 1 1 1 1
+  0 0 0 0 1
+  0 0 0 0 1
+  0 0 0 0 1
+  0 0 0 0 1
+  0 0 0 0 1
+  0 0 0 0 1
+  `;
   map.fill(0);
   const aster = new AStar({});
   const result = aster.findPath(new Point(0, 0), new Point(4, 6), map);
-  console.log(result.map(point => point.key));
+  result.forEach(point => map.put(point, 1));
+  assert(`${map}` === matcher);
 });
 
 test("flat routing: Always", () => {
   const map = new Map<number>(5, 7);
+  const matcher = stripIndents`
+  1 0 0 0 0
+  0 1 0 0 0
+  0 0 1 0 0
+  0 0 0 1 0
+  0 0 0 0 1
+  0 0 0 0 1
+  0 0 0 0 1
+  `;
   map.fill(0);
   const aster = new AStar({ diagonalMovement: Always });
   const result = aster.findPath(new Point(0, 0), new Point(4, 6), map);
-  console.log(result.map(point => point.key));
+  result.forEach(point => map.put(point, 1));
+  assert(`${map}` === matcher);
 });
 
 test("diagonal move: Never", () => {
   const map = new Map<number>(2, 2);
+  const matcher = stripIndents`
+  1 1
+  0 1
+  `;
   map.fill(0);
   const aster = new AStar({});
   const result = aster.findPath(new Point(0, 0), new Point(1, 1), map);
-  console.log(result.map(point => point.key));
-  assert(result.length === 3);
+  result.forEach(point => map.put(point, 1));
+  assert(`${map}` === matcher);
 });
 
 test("diagonal move: Always", () => {
   const map = new Map<number>(2, 2);
+  const matcher = stripIndents`
+  1 0
+  0 1
+  `;
   map.fill(0);
   const aster = new AStar({ diagonalMovement: Always });
   const result = aster.findPath(new Point(0, 0), new Point(1, 1), map);
-  console.log(result.map(point => point.key));
-  assert(result.length === 2);
+  result.forEach(point => map.put(point, 1));
+  assert(`${map}` === matcher);
 });
