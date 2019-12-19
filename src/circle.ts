2427fd7aade2b11be6bc0a18a5772420b4483473
Favi_ty <favius983@gmail.com>
2019-12-19 19:21:44 +0900

    wip

--- src/circle.ts
+++ src/circle.ts
@@ -1,5 +1,6 @@
 import Point from "./Point";
 
+//see: https://www.redblobgames.com/grids/circle-drawing/
 export function insideCircle(center: Point, target: Point, radius: number) {
   const distance = center.sub(target);
   const squared = distance.mul(distance);
