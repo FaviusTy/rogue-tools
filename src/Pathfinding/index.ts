cc5402634206609a8b48eca7a3b48ab389e05994
Favi_ty <favius983@gmail.com>
2020-01-18 00:42:37 +0900

    feat: index

---
+++ src/Pathfinding/index.ts
@@ -0,0 +1,7 @@
+import * as DiagonalMovement from "./DiagonalMovement.js";
+
+export { default as AStar } from "./AStar.js";
+export { DiagonalMovement };
+export { default as Heuristic } from "./Heuristic.js";
+export { default as NodeBucket } from "./NodeBucket.js";
+export { default as Node, NONE, OPEN, CLOSE } from "./Node.js";
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as matrix from "./matrix.js";
+import * as Pathfinding from "./Pathfinding/index.js";
+
+export { default as Point, point } from "./Point.js";
+export * from "./DataGrid.js";
+export { default as Neighbors, NeighborIndexes } from "./Neighbors.js";
+
+export { matrix };
+export { Pathfinding };
